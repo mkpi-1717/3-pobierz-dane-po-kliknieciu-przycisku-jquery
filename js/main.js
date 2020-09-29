d8f14cc67baa05192f0018f2cef4ca95c9715956
mkpi-1717 <69806641+mkpi-1717@users.noreply.github.com>
2020-09-29 20:11:47 +0200

    repozytorium

---
+++ js/main.js
@@ -0,0 +1,61 @@
+console.log("warsztat - 3-pobierz-dane-po-kliknieciu-przycisku-jquery");
+
+$(document).ready(function(){
+
+    // $('#get-data').click(function(){
+
+
+    //     $.get('https://akademia108.pl/api/ajax/get-post.php')
+    //         .done(function(data){
+    //             let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+    //             let pUserId = $('<p></p>').text(`Post ID: ${data.userId}`);
+    //             let pTitle = $('<p></p>').text(`Post ID: ${data.title}`);
+    //             let pBody = $('<p></p>').text(`Post ID: ${data.body}`);
+    //             let hr = $('<hr>');
+
+    //            let jqBody = $('body');
+
+    //            jqBody.append(pId);
+    //            jqBody.append(pUserId);
+    //            jqBody.append(pTitle);
+    //            jqBody.append(pBody);
+    //            jqBody.append(hr);
+
+    //         })
+    //         .fail(function(error){
+    //             console.error(error);
+    //         });
+    // });
+
+    $('#get-data').click(function(){
+
+
+        $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
+            .done(function(data){
+                let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+                let pUserId = $('<p></p>').text(`Post ID: ${data.userId}`);
+                let pTitle = $('<p></p>').text(`Post ID: ${data.title}`);
+                let pBody = $('<p></p>').text(`Post ID: ${data.body}`);
+                let hr = $('<hr>');
+
+               let jqBody = $('body');
+
+               jqBody.append(pId);
+               jqBody.append(pUserId);
+               jqBody.append(pTitle);
+               jqBody.append(pBody);
+               jqBody.append(hr);
+
+            })
+            .fail(function(error){
+                console.error(error);
+            });
+    });
+
+
+
+
+
+
+
+});
